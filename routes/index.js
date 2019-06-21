39dd1d6b89e4ffb7f6b881544972494c8ab814b5
Eagle19243 <eagle19243@gmail.com>
2019-06-21 12:42:06 +0300

    implement sftp test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express   = require('express');
+const router    = express.Router();
+const Client    = require('ssh2').Client;
+const conn      = new Client(); 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index');
 });
+
+router.post('/sftp_test', (req, res, next) => {
+    const host      = req.body.host;
+    const port      = req.body.port;
+    const username  = req.body.username;
+    const password  = req.body.password;
+    
+    conn.on('ready', function() {
+        conn.sftp(function(err, sftp) {
+            if (err) {
+                res.send({ success: false, error: err.message });
+            } else {
+                res.send({ success: true });
+            }
+        });
+    }).connect({
+        host: host,
+        port: port,
+        username: username,
+        password: password
+    });
+});
+
+// router.post('/sftp_upload', (req, res, next) => {
+    
+// });
 
 module.exports = router;
