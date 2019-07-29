9c2886bbd3461d7c76c448d8b1b1a63af7fc7b79
Eagle19243 <eagle19243@gmail.com>
2019-07-29 19:52:39 +0300

    implement sftp uploading

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express    = require('express');
 const router     = express.Router();
 const SFTPClient = require('ssh2').Client;
-const FTPClient  = require('ftp');;
+const FTPClient  = require('ftp');
+const Readable   = require('stream').Readable;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -47,8 +48,65 @@ router.post('/sftp_test', (req, res, next) => {
     }
 });
 
-// router.post('/sftp_upload', (req, res, next) => {
+router.post('/sftp_upload', (req, res, next) => {
+    const isSFTP    = req.body.is_sftp;
+    const host      = req.body.host;
+    const port      = req.body.port;
+    const username  = req.body.username;
+    const password  = req.body.password;
+    const content   = req.body.content;
+    const filename  = req.body.filename;
     
-// });
+    if (isSFTP > 0) {
+        const conn = new SFTPClient(); 
+        conn.on('ready', () => {
+            conn.sftp((err, sftp) => {
+                if (err) {
+                    console.log('SFTP error:', err);
+                    res.send({ success: false, error: err.message });
+                } else {
+                    const writeStream = sftp.createWriteStream(`/${filename}`);
+                    writeStream.on(
+                        'close',
+                        function () {
+                            console.log( "File transferred" );
+                            sftp.end();
+                            res.send({ success: true, url: `${host}/${filename}` });
+                        }
+                    );
+                    writeStream.write(content);
+                    writeStream.end();
+                }
+            });
+        }).connect({
+            host: host,
+            port: port,
+            username: username,
+            password: password
+        });
+    } else {
+        const encryption = req.body.encryption;
+        const conn = new FTPClient();
+        conn.on('ready', () => {
+            conn.put(content, `/${filename}`, (err) => {
+                if (err) {
+                    console.log('FTP error:', err);
+                    res.send({ success: false, error: err.message });
+                } else {
+                    console.log( "File transferred" );
+                    res.send({ success: true, url: `${host}/${filename}` });
+                }
+
+                conn.end();
+            });
+        }).connect({
+            host: host,
+            port: port,
+            user: username,
+            password: password,
+            secure: encryption > 0
+        });
+    }
+});
 
 module.exports = router;
