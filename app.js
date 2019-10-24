6a948c049c29f45e20354bea3af78e196e571d82
Eagle19243 <eagle19243@gmail.com>
2019-10-24 23:55:31 +0800

    fix issue

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use(cookieParser());
 
 app.use('/', require('./routes/index'));
