69ad8bf70256c0e884dfd8acf97b90ac7d53cd53
Eagle19243 <eagle19243@gmail.com>
2020-07-14 02:33:23 +0800

    get messages

--- routes/message.js
+++ routes/message.js
@@ -1,12 +1,19 @@
 const express    = require('express');
 const router     = express.Router();
+const ringcentral = require('../utils/ringcentral');
 
 /**
  * Return messages by phone number
- * if no phone number, return all messages
  */
-router.post('/get', (req, res) => {
-    const rcAuthData = req.body.rcAuthData;
+router.post('/get', async (req, res) => {
+    const rcAuthData  = req.body.rcAuthData;
+    const phoneNumber = req.body.phoneNumber;
+    const messages    = await ringcentral.getMessages(phoneNumber, rcAuthData);
+    
+    res.send({
+        success     : true,
+        messages    : messages
+    });
 });
 
 module.exports = router;
--- utils/ringcentral.js
+++ utils/ringcentral.js
@@ -9,7 +9,67 @@ const rcsdk      = new rcSDK({
 const rcPlatform = rcsdk.platform();
 
 const ringcentral = {
+    getMessages
+}
+
+async function getContacts(rcAuthData) {    
+    rcPlatform.auth().setData(rcAuthData);
+
+    try {
+        const response = await rcPlatform.get(`/restapi/v1.0/account/~/extension/~/address-book/contact`);
+        const jsonObj = response.json();
+        return jsonObj.records;
+    } catch (err) {
+        console.log('getContacts', err.message);
+        return [];
+    }
+}
+
+async function getMessageList(rcAuthData) {        
+    rcPlatform.auth().setData(rcAuthData);
+
+    try {
+        const response = await rcPlatform.get(`/restapi/v1.0/account/~/extension/~/message-store`);
+        const jsonObj = response.json();
+        return jsonObj.records;
+    } catch(err) {
+        console.log('getMessageList', err.message);
+        return [];
+    }
+}
+
+async function getMessages(phoneNumber, rcAuthData) {
+    // const contacts = await getContacts(rcAuthData);
+    const messages = await getMessageList(rcAuthData);
+    const result   = {};
     
+    for (const data of messages) {
+        if (data['from']['phoneNumber'] != phoneNumber) {
+            if (result[data['from']['phoneNumber']] != undefined) {
+                result[data['from']['phoneNumber']].push(data);
+            } else {
+                result[data['from']['phoneNumber']] = [data];
+            }
+        }
+
+        for (const record of data["to"]) {
+            const phone_number = record["phoneNumber"];
+
+            if (result[phone_number] != undefined) {
+                result[phone_number].push(data);
+            } else {
+                result[phone_number] = [data];
+            }
+        }
+    }
+
+    for (const key in result) {
+        result[key] = result[key].sort((a, b) => {
+            return new Date(a.creationTime).getTime() - new Date(b.creationTime).getTime();
+        });
+    }
+
+    return result;
 }
 
 module.exports = ringcentral;
